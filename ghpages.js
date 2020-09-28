5c36e4c842e76e65bf668cc4631398a07cd3fde7
Christopher Thomas <christopher.bird.thomas@gmail.com>
2020-09-27 23:41:18 -0700

    Update ghpages to fix absolute links with /website prefix before publishing

--- ghpages.js
+++ ghpages.js
@@ -1,5 +1,22 @@
 var ghpages = require('gh-pages');
- 
-ghpages.publish('_dist', {
+var glob = require('glob');
+var fs = require('fs');
 
-}, function(err) {});
+const allFiles = glob.sync('_dist/**/*').filter((file) => /\.(js|html)$/ig.test(file));
+//console.log(allFiles);
+
+const pathMatcher = /(?<=['"])(\/[\w\/_\-]+\.(js|html|png|jpg|gif|css))(?=['"])/igm;
+
+allFiles.forEach(file => {
+  let content = fs.readFileSync(file, {encoding: 'utf8'});
+  //let matches = content.match(pathMatcher);
+  //console.log(matches);
+
+  content = content.replace(pathMatcher, '/website$1')
+  
+  // matches = content.match(pathMatcher);
+  console.log(content);
+  fs.writeFileSync(file, content, {encoding: 'utf8'});
+});
+
+ghpages.publish('_dist', {}, function(err) {});
