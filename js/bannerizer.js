b95f2fa9aca2e5f822e367c63e96c084a9e3a76a
Christopher Thomas <christopher.bird.thomas@gmail.com>
2020-09-27 23:32:48 -0700

    Updates

--- js/bannerizer.js
+++ js/bannerizer.js
@@ -1,9 +1,9 @@
 setTimeout(function () {
   var bannerImg = document.getElementById('banner');
   var bgImages = [
-    "/images/banners/ocean1.jpg",
-"/images/banners/shutterstock_366857099.jpg",
-"/images/banners/Thinking-of-getting-a-cat.png"
+    "/website/images/banners/ocean1.jpg",
+"/website/images/banners/shutterstock_366857099.jpg",
+"/website/images/banners/Thinking-of-getting-a-cat.png"
   ];
 
   bannerImg.src = bgImages[((Math.random(Date.now()) * bgImages.length) >> 0)];
